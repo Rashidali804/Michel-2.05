8180033cc48cf7d6c357dcab31298170f3c87c2f
Rashidali804 <rs.alizardari@gmail.com>
2026-05-07 12:08:26 +0500

    Create images.js

---
+++ images.js
@@ -0,0 +1,16 @@
+module.exports = [
+    // === ONLINE IMAGES ===
+    "https://i.imgur.com/9zU3y7K.jpeg",
+    "https://i.imgur.com/Yo6mQJd.jpeg", 
+    "https://i.imgur.com/8pXkLqE.jpeg",
+    "https://i.imgur.com/kT5wGvN.jpeg",
+    "https://i.imgur.com/2nFhP3s.jpeg",
+    
+    // === LOCAL IMAGES ===
+    // Apne PC mein "dp" naam ka folder banao aur pics rakho
+    "./dp/pic1.jpg",
+    "./dp/pic2.png", 
+    "./dp/pic3.jpg",
+    "./dp/rashid.jpg",
+    "./dp/michel.jpg"
+];
